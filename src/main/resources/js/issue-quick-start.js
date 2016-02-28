ec794d326b8941a14807cbe8d8f21bd71cf421b9
Andrew Heald <andrew@heald.co.uk>
2016-02-28 19:14:27 +0000

    Further JS code change suggested by Robert Wolf.

--- src/main/resources/js/issue-quick-start.js
+++ src/main/resources/js/issue-quick-start.js
@@ -1,11 +1,24 @@
 /*
-    This code almost taken verbatim from Jared Farrish's comments at https://confluence.atlassian.com/display/JIRA/Creating+an+Issue.
+    This code almost taken verbatim from Jared Farrish's comments at
+    https://confluence.atlassian.com/display/JIRA/Creating+an+Issue.
     Jared's comments are taken as implied consent to open source use in this context.
+    A further change was suggested in Robert Wolf's plugin review at
+    https://marketplace.atlassian.com/plugins/uk.org.sappho.jira.plugin.jira-plugin-goto-new-issue/server/reviews.
  */
+
 AJS.$(document).ready(function(){
     if (JIRA && JIRA.Forms) {
         JIRA.Forms.UnconfigurableCreateIssueForm = JIRA.Forms.UnconfigurableCreateIssueForm.extend({
-            handleSubmitSuccess: function(issue){
+            handleSubmitSuccess: function(issue) {
+                if (this.helper.getCreateAnotherCheckbox().is(":checked")) {
+                    this.helper.handleSubmitSuccess(issue);
+                } else {
+                    window.location = (contextPath.length > 0 ? contextPath : "") + "/browse/" + issue.issueKey;
+                }
+            }
+        });
+        JIRA.Forms.ConfigurableCreateIssueForm = JIRA.Forms.ConfigurableCreateIssueForm.extend({
+            handleSubmitSuccess: function(issue) {
                 if (this.helper.getCreateAnotherCheckbox().is(":checked")) {
                     this.helper.handleSubmitSuccess(issue);
                 } else {
